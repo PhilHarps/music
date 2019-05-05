c4af0c60db29785feb8483afb7bf0fed0112506c
rachelwong <hello.rachelwong@gmail.com>
2019-05-05 11:46:43 +1000

    create edit show user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
 
   def show
+    # show listing of user (profile page doubles up as own listings page)
+  end
+
+  def edit
+    # edit profile
+    @user = User.find(params[:id])
+  end
+
+  def update
+    current_user.update(user_params)
+    redirect_to current_user
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :address, :state, :city)
   end
 end
