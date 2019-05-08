026863d53915ec013ec0cfc52e06894e931b3060
rachelwong <hello.rachelwong@gmail.com>
2019-05-08 21:15:16 +1000

    school boolean migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_05_050951) do
+ActiveRecord::Schema.define(version: 2019_05_08_111026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2019_05_05_050951) do
     t.text "address"
     t.string "city"
     t.string "state"
+    t.boolean "is_school", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
