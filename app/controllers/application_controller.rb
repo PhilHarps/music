5173b2ddc3cbd3a9939baac7c01c4f418c3fa317
rachelwong <hello.rachelwong@gmail.com>
2019-05-09 12:18:32 +1000

    changed header for user type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,14 @@
 class ApplicationController < ActionController::Base
 
-     before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
   # sanitize extra fields' data before it hits model
   def configure_permitted_parameters
-
     # for sign up form
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :city, :state, :email, :is_school])
 
     # for edit profile form
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address, :city, :state, :email, :is_school])
   end
-
 end
